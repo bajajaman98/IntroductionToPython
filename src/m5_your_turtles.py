e1fc01e8a5569f317504a0387a66ba0ee24c61c3
bajaja <bajaja@rose-hulman.edu>
2017-11-27 09:49:53 -0500

    I did the todos

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Aman Bajaj.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,32 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow
+
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen = rg.Pen('blue',10)
+blue_turtle.speed = 10
+circle_size = 250
+blue_turtle.pen_up()
+blue_turtle.go_to(rg.Point(0,-200))
+blue_turtle.pen_down()
+
+red_turtle = rg.SimpleTurtle('turtle')
+red_turtle.pen = rg.Pen('red',5)
+red_turtle.speed = 5
+square_size = 50
+red_turtle.pen_up()
+red_turtle.go_to(rg.Point(-15,-165))
+red_turtle.pen_down()
+for k in range(10):
+
+    blue_turtle.draw_circle(circle_size)
+    circle_size -= 20
+    red_turtle.draw_square(square_size)
+    red_turtle.pen_up()
+    red_turtle.right(90)
+    red_turtle.backward(30)
+    red_turtle.pen_down()
+
+window.close_on_mouse_click()
